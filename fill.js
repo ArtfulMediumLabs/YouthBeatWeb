9afbe0f81b9920cdd1bed09654df68e369d582b6
Greg Cerveny <greg.cerveny@gmail.com>
2019-10-31 10:52:43 -0500

    cleaning up presets

--- fill.js
+++ fill.js
@@ -61,3 +61,15 @@ if (!String.prototype.padStart) {
       }
   };
 }
+
+if (!Object.entries) {
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    
+    return resArray;
+  };
+}
