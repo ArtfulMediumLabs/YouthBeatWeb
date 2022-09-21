d07e8f1eaa01167ad407ac95ccea2539263f5bef
Greg Cerveny <greg.cerveny@gmail.com>
2022-09-21 14:03:59 -0400

    extract language code to match lessons

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-
-const langElement = document.querySelector("#lang");
-var english, french;
-if (lang == 'en') {
-  english = document.createElement('span');
-  english.innerText = "English";
-
-  french = document.createElement('a')
-  french.href = "";
-  french.innerText = "Français";
-  french.onclick = updateLanguage.bind(null, 'fr');
-} else {
-  english = document.createElement('a');
-  english.href = "";
-  english.onclick = updateLanguage.bind(null, 'en');
-  english.innerText = "English";
-
-  french = document.createElement('span')
-  french.innerText = "Français";
-}
-langElement.append(english)
-langElement.append(" : ")
-langElement.append(french)
-
-function updateLanguage(code, event) {
-  event.preventDefault();
-  lang = code;
-  var url = new URL(window.location);
-  (url.searchParams.has('lang') ? url.searchParams.set('lang', code) : url.searchParams.append('lang', code));
-  console.log(url);
-  window.location.assign(url);
-}
-
 var offset = 0
 function currentBeat() {
   if (offset == 0) { return 0; }
